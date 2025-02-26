9a37e5cf798878afe14da91f9690f286acb1205c
Jascha S <jascha.schewtschenko@port.ac.uk>
2025-02-26 15:55:36 +0100

    exchanged websocket-based implementation with socketio-based

--- src/python/dandeliion/client/websocket.py
+++ src/python/dandeliion/client/websocket.py
@@ -23,65 +23,26 @@ Module for websocket client used in simulator
 #
 
 # built-in modules
-import threading
-import json
-import websocket
 import logging
-from threading import Condition
 from collections.abc import Callable
 from typing import Any
 
+# third-party modules
+import socketio
 
 logger = logging.getLogger(__name__)
 
 
 class SimulatorWebSocketClient:
 
-    def __init__(self, url: str, api_key: str, on_update: Callable[[Any], None]):
-        headers = {'Authorization': f'Token {api_key}'}  # TODO adapt to server
+    def __init__(self, url: tuple[str, str], api_key: str, on_update: Callable[[Any], None]):
+        server_url, self.namespace = url
+        self._app = socketio.Client()
+        self._app.connect(server_url, auth=api_key, namespaces=[self.namespace])
+        self._app.on('update', namespace=self.namespace)(on_update)
 
-        self._app = websocket.WebSocketApp(
-            url,
-            on_open=self._on_open,
-            on_message=self._on_message,
-            on_error=self._on_error,
-            on_close=self._on_close,
-            header=headers,
-        )
-
-        self._on_update = on_update
-        self._is_opened = False
-        self._is_ready = Condition()
-
-        # Initialise the run_forever inside a thread and make this thread as a daemon thread
-        wst = threading.Thread(target=self._app.run_forever)
-        # wst.daemon = True  # not needed anymore?
-        wst.start()
-
-    def send_message(self, message):
-        with self._is_ready:
-            self._is_ready.wait_for(lambda: self._is_opened)
-        self._app.send(message)
-
-    def subscribe(self, run_id):
-        self.send_message(run_id)
+    def subscribe(self, id):
+        self._app.emit('subscribe', id, namespace=self.namespace)
 
     def close(self):
-        self._app.close()
-
-    def _on_open(self, wsapp):
-        with self._is_ready:
-            self._is_opened = True
-            self._is_ready.notify_all()
-
-    def _on_message(self, wsapp, message) -> None:
-        self._on_update(json.loads(message)['updates'])
-
-    def _on_close(self, wsapp, close_status_code, close_msg):
-        # Because on_close was triggered, we know the opcode = 8
-        logger.debug("on_close args:")
-        logger.debug("close status code: " + str(close_status_code))
-        logger.debug("close message: " + str(close_msg))
-
-    def _on_error(self, wsapp, err):
-        logger.error("ERROR", wsapp, err)
+        self._app.shutdown()
