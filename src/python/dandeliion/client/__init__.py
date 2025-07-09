b3a39a1811fcf526b7b5cec42d0fe30938611c99
Jascha S <jascha.schewtschenko@port.ac.uk>
2025-07-10 00:55:29 +0200

    pybamm now optional; drive cycles introduced & local example updated to include this

--- src/python/dandeliion/client/__init__.py
+++ src/python/dandeliion/client/__init__.py
@@ -2,36 +2,61 @@
 import importlib.metadata
 from pathlib import Path
 from typing import Union
+from collections.abc import Sequence
 
 # custom modules
 from .simulator import Simulator
 from .solution import Solution
 
 # third-party modules
-from pybamm import Experiment
+try:
+    from pybamm import Experiment, Interpolant
+    from pybamm.experiment.step import Power, Current, Voltage, BaseStep
+    __has_pybamm__ = True
+except ModuleNotFoundError:
+    from .experiment import Experiment
+    __has_pybamm__ = False
 from bpx import parse_bpx_obj, parse_bpx_file, BPX
 
 __version__ = importlib.metadata.version('dandeliion-client')
 
 
-def _convert_experiment(experiment: Experiment):
+def _convert_experiment(experiment: Experiment) -> tuple[dict, dict]:
     """
     converts pybamm experiment into dict
     """
     operating_conditions, period, temperature, termination = experiment.args
     steps = []
+    time_series = None
     for cond in operating_conditions:
         if isinstance(cond, tuple):
             steps += list(cond)
-        else:
+        elif isinstance(cond, str):
             steps.append(cond)
+        elif __has_pybamm__ and isinstance(cond, (Current, Power)):
+            if isinstance(cond.value, Interpolant):
+                if cond.input_duration is not None:
+                    raise TypeError("'duration' argument not supported for drive cycles yet.")
+                time_series = {
+                    'Time [s]': list(cond.value.x[0] if isinstance(cond.value.x, Sequence)
+                                     else cond.value.x)
+                }
+                if isinstance(cond, Current):
+                    time_series['Current [A]'] = list(cond.value.y)
+                else:
+                    time_series['Power [W]'] = list(cond.value.y)
+                steps.append('Time series')
+            else:
+                raise TypeError(f"{type(cond)} only supported as drive cycle at the moment.")
+        else:
+            raise TypeError(f"Unsupported type found in Experiment: {type(cond)}")
 
     return {
         "Instructions": steps,
         "Period": period,
         "Temperature": temperature,
         "Termination": termination,
-    }
+    }, time_series
 
 
 def solve(
@@ -79,7 +104,15 @@ def solve(
 
     # add experiment
     if experiment:
-        params['Parameterisation']["User-defined"]["DandeLiion: Experiment"] = _convert_experiment(experiment)
+        experiment_, time_series = _convert_experiment(experiment)
+        params['Parameterisation']["User-defined"]["DandeLiion: Experiment"] = experiment_
+        if time_series is not None:
+            # check if time series already exists and only accept it if identical
+            if "DandeLiion: Time series input" in params['Parameterisation']["User-defined"]:
+                if params['Parameterisation']["User-defined"]["DandeLiion: Time series input"] != time_series:
+                    raise ValueError("Currently only identical drive cycles are supported")
+            else:
+                params['Parameterisation']["User-defined"]["DandeLiion: Time series input"] = time_series
 
     # add/overwrite extra parameters
     if extra_params:
